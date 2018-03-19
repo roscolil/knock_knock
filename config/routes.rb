9e782f2fe185008d15cd3b30851f8a12eb85e0c2
roscolil <ross.lillis@gmail.com>
2018-03-19 13:20:11 +1100

    added api routes and respective controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+get '/api/Fibonacci', to: 'finbonacci#api_index'
+
+get '/api/ReverseWords', to: 'reverse_words#api_index'
+
+get '/api/Token', to: 'tokens#api_index'
+
+get '/api/TriangleType', to: 'triangle_types#api_index'
