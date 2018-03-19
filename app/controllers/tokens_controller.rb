0fc9aceb73e5d72243acd48f5cd91a9b88f8128e
roscolil <ross.lillis@gmail.com>
2018-03-19 20:37:36 +1100

    completed token & triangle methods

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,8 +1,7 @@
 class TokensController < ApplicationController
 
   def api_index
-    
-
+    res = '89b3ca02-4910-420b-80aa-2a75b052a747'
 
     render json: res
   end
--- app/controllers/triangle_types_controller.rb
+++ app/controllers/triangle_types_controller.rb
@@ -1,7 +1,17 @@
 class TriangleTypesController < ApplicationController
 
   def api_index
+    a = params[:a]
+    b = params[:b]
+    c = params[:c]
 
+    if ((a == b) && (a == c) && (b == c))
+      res = 'equilateral'
+    elsif ((a == b) || (a == c) || (b == c))
+      res = 'isosceles'
+    else
+      res = 'scalene'
+    end
+    render json: res
   end
-
 end
